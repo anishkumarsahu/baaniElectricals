09007ec0b405bb9dd16dece722f85a350b74fdb6
anishkumarsahu <anishkumarsahu03@gmail.com>
2022-11-25 16:56:43 +0530

    completed the project.

--- baani/settings.py
+++ baani/settings.py
@@ -76,27 +76,27 @@ WSGI_APPLICATION = 'baani.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'baani',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'OPTIONS': {
-            "init_command": "SET foreign_key_checks = 0;",
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'baani',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#         'OPTIONS': {
+#             "init_command": "SET foreign_key_checks = 0;",
+#             'charset': 'utf8mb4',
+#         },
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
