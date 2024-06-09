3878f30684423a4510f44358c51b8e81fa7b0a08
anishkumarsahu <anishkumarsahu03@gmail.com>
2024-06-09 14:39:20 +0530

    changes whatsapp url

--- home/apiView.py
+++ home/apiView.py
@@ -18,7 +18,7 @@ from weasyprint import CSS, HTML
 
 from home.models import *
 
-BASE_URL_WHATSAPP = "https://node3.betablaster.in/api/"
+BASE_URL_WHATSAPP = "https://web.betablaster.in/api/"
 
 
 class LocationThread(threading.Thread):
